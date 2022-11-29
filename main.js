2000202045073f7b13b2940d93521c70c03cbb55
Michel Maia <programador.michel.maia@gmail.com>
2022-11-29 16:39:26 -0300

    commit

---
+++ main.js
@@ -0,0 +1,24 @@
+function tocaSom(idElAudio) {
+    document.querySelector(idElAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    } 
+    
+    contador = contador +1;
+
+}
+
+
+
