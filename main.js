6ae2d58a26ab11390a9067e46e211adc9e19c9d9
Michel Maia <programador.michel.maia@gmail.com>
2022-11-30 10:39:55 -0300

    commit

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-function tocaSom(idElAudio) {
-    document.querySelector(idElAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Não encontrado');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-while (contador < listaDeTeclas.length) {
+
+for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
@@ -15,8 +21,19 @@ while (contador < listaDeTeclas.length) {
     tecla.onclick = function() {
         tocaSom(idAudio);
     } 
-    
-    contador = contador +1;
+
+    tecla.onkeydown = function(evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Tab' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+        
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
+
 
 }
 
